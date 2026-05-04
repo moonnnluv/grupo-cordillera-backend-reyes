eba8b8f08f9105400f82b146e7cfa7256ea6234d
moonnnluv <ale.reyesd@duocuc.cl>
2026-05-04 11:32:17 -0400

    feat: estructura base inicial para microservicio ms-bff

---
+++ msbff/src/main/java/com/grupcordillera/msbff/MsbffApplication.java
@@ -0,0 +1,13 @@
+package com.grupcordillera.msbff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsbffApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsbffApplication.class, args);
+	}
+
+}
